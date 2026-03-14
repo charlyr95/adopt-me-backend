c176c81e971a5845268cf5ea1ffe7795f8c1ed0c
Carlos Daniel Romero <carloscr95r@gmail.com>
2026-03-14 15:49:35 -0300

    warn

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,9 +1,19 @@
 import { logger } from '../utils/logger.js';
 
 export const loggerMiddleware = (req, res, next) => {
-  logger.info(`${req.method} ${req.originalUrl}`, {
-    ip: req.ip,
-    userAgent: req.get('user-agent')
+  res.on('finish', () => {
+    const logData = {
+      ip: req.ip,
+      userAgent: req.get('user-agent'),
+      statusCode: res.statusCode
+    };
+
+    if (res.statusCode === 401 || res.statusCode === 403) {
+      logger.warn(`${req.method} ${req.originalUrl}`, logData);
+      return;
+    }
+
+    logger.info(`${req.method} ${req.originalUrl}`, logData);
   });
 
   next();
